47e4d4f4db850493d7ac657ee0b832b542e09e5f
Daniel Wirzba <s22617@pjwstk.edu.pl>
2022-06-18 13:58:32 +0200

    Added swagger

--- src/main/java/com/s22617/jazs22617nbp/configuration/NbpConfiguration.java
+++ src/main/java/com/s22617/jazs22617nbp/configuration/NbpConfiguration.java
@@ -3,6 +3,14 @@ package com.s22617.jazs22617nbp.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class NbpConfiguration {
@@ -10,5 +18,26 @@ public class NbpConfiguration {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket get() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.s22617.nbpevaluator"))
+                .build().apiInfo(createApiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("NBP Evaluator",
+                "REST API providing average gold price from NBP",
+                "1.0",
+                "",
+                new Contact("Daniel", "https://github.com/s22617", "s22617@pjwstk.edu.pl"),
+                "",
+                "",
+                Collections.emptyList());
     }
 }
